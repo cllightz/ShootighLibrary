abd7d948a856fe11ee10ae1961ccfabb71bcb81a
cllightz <cllightz@gmail.com>
2017-04-24 23:57:06 +0900

    delete SampleControl

--- ShootighLibrary/Sample/Ball.cs
+++ ShootighLibrary/Sample/Ball.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using SharpDX.Direct2D1;
+﻿using SharpDX.Direct2D1;
 using ShootighLibrary;
 
 namespace Sample
--- ShootighLibrary/Sample/SampleGame.cs
+++ ShootighLibrary/Sample/SampleGame.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 using SharpDX.Direct2D1;
 using SharpDX.Mathematics.Interop;
 using ShootighLibrary;
